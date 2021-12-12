65a80a0a03e18546ecec51ef4ba7831908a64f38
Alex Musayev <alex.musayev@gmail.com>
2021-12-12 20:18:12 +0300

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -30,14 +30,6 @@ amplifr_logs() {
   kubectl --kubeconfig ~/.kube/config logs -f -n $1 --context amplifr $(amplifr_name $1)
 }
 
-# alias amplifr_prod_railsc="amplifr_exec 'amplifr-app' 'bundle exec rails c'"
-# alias amplifr_prod_bash="amplifr_exec 'amplifr-app' 'bundle exec /bin/bash'"
-# alias amplifr_prod_rr="amplifr_exec 'amplifr-app' 'bundle exec rails r -'"
-
-# alias amplifr_stag_railsc="amplifr_exec 'amplifr-staging' 'bundle exec rails c'"
-# alias amplifr_stag_bash="amplifr_exec 'amplifr-staging' 'bundle exec bash'"
-# alias amplifr_stag_rr="amplifr_exec 'amplifr-staging' 'bundle exec rails r -'"
-
 # Production
 alias aprailsc="amplifr_exec 'amplifr-app' 'bundle exec rails c'"
 alias apbash="amplifr_exec 'amplifr-app' 'bundle exec /bin/bash'"
