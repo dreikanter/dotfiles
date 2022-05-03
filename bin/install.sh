69f4cf6c3cd2e8fc8d116fbdaaff2e07efc86046
Alex Musayev <alex.musayev@gmail.com>
2022-05-03 21:53:40 +0700

    Script paths

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,7 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-~/.dotfiles/install-homebrew.sh
-~/.dotfiles/install-links.sh
-~/.dotfiles/install-sublime.sh
-~/.dotfiles/macos-defaults.sh
+~/.dotfiles/bin/install-homebrew.sh
+~/.dotfiles/bin/install-links.sh
+~/.dotfiles/bin/install-sublime.sh
+~/.dotfiles/bin/macos-defaults.sh
