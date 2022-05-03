a72024c9be800150c823aa4e8339f2e4b557d799
Alex Musayev <alex.musayev@gmail.com>
2022-05-03 21:53:05 +0700

    Sublime

--- bin/install-sublime.sh
+++ bin/install-sublime.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-USER_DIR=~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+USER_DIR=~/Library/Application\ Support/Sublime\ Text/Packages/User
 
 if [ -d "$USER_DIR" ]; then
   if [ -L "$USER_DIR" ]; then
@@ -13,3 +13,4 @@ if [ -d "$USER_DIR" ]; then
 fi
 
 ln -sf ~/.dotfiles/sublime/User "$USER_DIR"
+
