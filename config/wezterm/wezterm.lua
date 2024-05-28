0889d549051ede03fe9c034c918e0653406899d1
Alex Musayev <alex.musayev@gmail.com>
2024-05-28 17:10:08 +0200

    Update Wezterm configuration

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,30 +1,23 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local action = wezterm.action
-local config = wezterm.config_builder()
 
--- wezterm.on('paste-selection', function(window, pane)
---     local sel = window:get_selection_text_for_pane(pane)
---     pane:send_text(sel)
--- end)
-
--- config.mouse_bindings = {
---     -- Disable automatic copy to buffer on selection
---     { event = { Up = { streak = 1, button = 'Left' } },   mods = 'NONE', action = action.Nop, },
---     { event = { Up = { streak = 2, button = 'Left' } },   mods = 'NONE', action = action.Nop, },
---     -- 3 stages of text selection
---     { event = { Down = { streak = 2, button = 'Left' } }, mods = 'NONE', action = action.SelectTextAtMouseCursor 'Word', },
---     { event = { Down = { streak = 3, button = 'Left' } }, mods = 'NONE', action = action.SelectTextAtMouseCursor 'Line', },
---     { event = { Down = { streak = 4, button = 'Left' } }, mods = 'NONE', action = action.SelectTextAtMouseCursor 'SemanticZone', },
--- }
-
-config.keys = {
-    -- Clear scrollback and viewport
-    -- { key = 'k', mods = 'CMD',       action = action.ClearScrollback 'ScrollbackAndViewport' },
-    -- Command palette
-    -- { key = 'p', mods = 'CMD', action = action.ActivateCommandPalette },
-    -- Insert current selection into the terminal
-    -- { key = 'v', mods = 'SUPER|SHIFT', action = wezterm.action.EmitEvent 'paste-selection' },
-    -- { key = 'v', mods = 'SUPER',       action = action.PasteFrom 'Clipboard' },
+return {
+  color_scheme = "AdventureTime",
+  font = wezterm.font("JetBrains Mono"),
+  font_size = 16.0,
+  window_close_confirmation = "NeverPrompt",
+  keys = {
+    { key = "l", mods = "CMD|SHIFT", action = action.ShowDebugOverlay },
+    {
+      key = "k",
+      mods = "CMD",
+      action = action.Multiple {
+        action.ClearScrollback("ScrollbackAndViewport"),
+        action.SendKey { key = "L", mods = "CTRL" },
+      },
+    },
+    { key = "f", mods = "CMD", action = action.Search("CurrentSelectionOrEmptyString"), },
+    { key = "q", mods = "CMD", action = action.QuitApplication },
+    { key = "w", mods = "CMD", action = action.CloseCurrentTab { confirm = false } },
+  },
 }
-
-return config
