861580b518be14248239662557ec0bf31d63462e
Alex Musayev <alex.musayev@gmail.com>
2024-06-17 13:29:07 +0200

    Use consistent script naming

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,13 +7,17 @@ hs.hotkey.bind({"cmd", "alt"}, "2", function()
 end)
 
 hs.hotkey.bind({"ctrl", "shift"}, "n", function()
-  hs.execute("~/.dotfiles/bin/newnote", true)
+  hs.execute("~/.dotfiles/bin/new-note", true)
 end)
 
 hs.hotkey.bind({"ctrl", "shift"}, "l", function()
-  hs.execute("~/.dotfiles/bin/latestnote", true)
+  hs.execute("~/.dotfiles/bin/latest-note", true)
 end)
 
 hs.hotkey.bind({"ctrl", "shift"}, "t", function()
-  hs.execute("~/.dotfiles/bin/latesttodo", true)
+  hs.execute("~/.dotfiles/bin/latest-todo", true)
+end)
+
+hs.hotkey.bind({"ctrl", "shift"}, "b", function()
+  hs.execute("~/.dotfiles/bin/latest-backlog", true)
 end)
