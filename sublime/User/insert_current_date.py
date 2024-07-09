8e48db31c7cd7c123773b28aab367b4e8f23cbe5
Alex Musayev <alex.musayev@gmail.com>
2024-07-09 20:53:13 +0200

    Fix `insert_current_date` command for Sublime

--- sublime/User/insert_current_date.py
+++ sublime/User/insert_current_date.py
@@ -2,7 +2,7 @@ import datetime
 import sublime_plugin
 
 
-class InsertDateCommand(sublime_plugin.TextCommand):
+class InsertCurrentDateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         timestamp_str = datetime.datetime.now().strftime("%Y-%m-%d:\n")
         for r in self.view.sel():
