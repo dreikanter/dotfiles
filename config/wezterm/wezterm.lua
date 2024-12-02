5a26247c305091147980ed84aa5cd0ef21c0d403
Alex Musayev <alex.musayev@gmail.com>
2024-12-02 12:11:34 +0100

    Update wezterm theme

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local wezterm = require("wezterm")
 local action = wezterm.action
 
+-- Color schemes
+-- https://wezfurlong.org/wezterm/colorschemes/a/index.html
+
 return {
-  -- color_scheme = "AdventureTime",
-  color_scheme = "Batman",
+  color_scheme = "Afterglow",
   font = wezterm.font("JetBrains Mono"),
   font_size = 16.0,
   window_close_confirmation = "NeverPrompt",
