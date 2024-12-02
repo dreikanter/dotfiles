6454a9e261cd5ccb9327bc29a3947421b273935f
Alex Musayev <alex.musayev@gmail.com>
2024-12-02 12:06:55 +0100

    Update wezterm theme

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require("wezterm")
 local action = wezterm.action
 
 return {
-  color_scheme = "AdventureTime",
+  -- color_scheme = "AdventureTime",
+  color_scheme = "Batman",
   font = wezterm.font("JetBrains Mono"),
   font_size = 16.0,
   window_close_confirmation = "NeverPrompt",
@@ -13,8 +14,9 @@ return {
       mods = "CMD",
       action = action.ClearScrollback("ScrollbackAndViewport"),
     },
-    { key = "f", mods = "CMD", action = action.Search("CurrentSelectionOrEmptyString"), },
+    { key = "f", mods = "CMD", action = action.Search("CurrentSelectionOrEmptyString") },
     { key = "q", mods = "CMD", action = action.QuitApplication },
-    { key = "w", mods = "CMD", action = action.CloseCurrentTab { confirm = false } },
+    { key = "w", mods = "CMD", action = action.CloseCurrentTab({ confirm = false }) },
+    { key = "a", mods = "CMD|SHIFT", action = action.QuickSelect },
   },
 }
