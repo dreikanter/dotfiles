43a0c093e6eeba64d50f35445c3340313deed18f
Alex Musayev <alex.musayev@gmail.com>
2024-12-29 13:45:47 +0100

    Update keyboard layout hotkeys

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,21 @@
+-- for idx, layout in pairs(hs.keycodes.layouts()) do
+--   hs.hotkey.bind({ "cmd", "alt" }, tostring(idx), function()
+--     hs.keycodes.setLayout(layout)
+--   end)
+-- end
+
 for idx, layout in pairs(hs.keycodes.layouts()) do
-  hs.hotkey.bind({ "cmd", "alt" }, tostring(idx), function()
-    hs.keycodes.setLayout(layout)
-  end)
+  if layout:find("English") and not englishBound then
+    hs.hotkey.bind({ "cmd", "alt" }, "1", function()
+      hs.keycodes.setLayout(layout)
+    end)
+    englishBound = true
+  elseif layout:find("Russian") and not russianBound then
+    hs.hotkey.bind({ "cmd", "alt" }, "2", function()
+      hs.keycodes.setLayout(layout)
+    end)
+    russianBound = true
+  end
 end
 
 hs.hotkey.bind({"ctrl", "shift"}, "n", function()
