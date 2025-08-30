bb05456af3509a0fb647a8cd6bc02f925b483359
Alex Musayev <alex.musayev@gmail.com>
2025-08-30 16:46:49 +0200

    Clean up

--- sublime/User/insert_inline_date.py
+++
@@ -1,9 +0,0 @@
-import datetime
-import sublime_plugin
-
-
-class InsertInlineDateCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        timestamp_str = datetime.datetime.now().strftime("(%Y-%m-%d)")
-        for r in self.view.sel():
-            self.view.insert(edit, r.begin(), timestamp_str)
