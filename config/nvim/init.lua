e35060b162749a1ee67f64d8ae33059f55d90626
Alex Musayev <alex.musayev@gmail.com>
2025-09-05 13:51:37 +0200

    Update Neovim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,56 +1,92 @@
--- Not much is going on:
--- - Install Telescope for fuzzy file search
--- - Install mini.statusline
--- - Install nice theme matching WezTerm
--- - Set Space for the leader key
--- - Define some key binding
+-- Not much is going on
 
--- leader must be set before mappings
-vim.g.mapleader = " "
+vim.g.mapleader = " " -- leader must be set before mappings
 vim.g.editorconfig = true
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.clipboard = "unnamedplus"
 
 -- plugins
+
 vim.pack.add {
-  { src = "https://github.com/nvim-lua/plenary.nvim" }, -- Telescope dep
-  { src = "https://github.com/nvim-telescope/telescope.nvim" },
   { src = "https://github.com/folke/tokyonight.nvim" },
   { src = "https://github.com/echasnovski/mini.nvim" },
+  { src = "https://github.com/ibhagwan/fzf-lua" },
+  { src = "https://github.com/lewis6991/gitsigns.nvim" }
 }
 
 -- plugins setup (safe if not yet installed)
+
 pcall(function()
-  require("telescope").setup({})
   vim.cmd.colorscheme("tokyonight-night")
-  require('mini.statusline').setup()
-  require('mini.diff').setup()
-  require('mini.comment').setup()
+
+  require("mini.statusline").setup()
+  require("mini.diff").setup()
+  require("mini.comment").setup()
+  require("mini.pick").setup()
+
+  require("fzf-lua").setup({
+    winopts = {
+      height = 0.85,
+      width = 0.90,
+      -- row = 0.35,
+      -- col = 0.50,
+      border = "rounded",
+      preview = {
+        layout = "vertical",
+        vertical = "down:60%",
+      },
+    },
+    keymap = {
+      builtin = {
+        ["<C-d>"] = "preview-page-down",
+        ["<C-u>"] = "preview-page-up",
+      },
+    },
+    fzf_opts = {
+      ["--layout"] = "default",
+      ["--info"] = "inline",
+    },
+    files = {
+      git_icons = false,
+      file_icons = false,
+      color_icons = false,
+      fd_opts = "--color=never --type f --hidden --follow --exclude .git",
+      find_opts = [[-type f -not -path '*/\.git/*' -printf '%P\n']],
+    },
+    grep = {
+      rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case --max-columns=512",
+    },
+  })
+
+  require('gitsigns').setup {
+    signcolumn = false,
+    current_line_blame = true
+  }
 end)
 
 -- keymaps
-local telescope_builtin = function(name)
-  return function()
-    require("telescope.builtin")[name]()
-  end
-end
 
--- telescope
-vim.keymap.set("n", "<leader>f", telescope_builtin("find_files"), { silent = true, desc = "Find files" })
-vim.keymap.set("n", "<leader>g", telescope_builtin("live_grep"),  { silent = true, desc = "Grep project" })
-vim.keymap.set("n", "<leader>b", telescope_builtin("buffers"),    { silent = true, desc = "Buffers" })
+map = vim.keymap.set
 
--- general purpose
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { silent = true, desc = "Save file" })
-vim.keymap.set("n", "<leader>r", "<cmd>luafile ~/.config/nvim/init.lua<CR><cmd>echo 'Reloaded!'<CR>", { desc = "Reload config" })
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>", { silent = true, desc = "Clear search highlights" })
+map("n", "<leader>tw", function()
+  vim.wo.wrap = not vim.wo.wrap
+  vim.wo.linebreak = vim.wo.wrap
+  vim.notify(vim.wo.wrap and "Word wrap" or "No wrap")
+end, { desc = "Toggle word wrap" })
 
--- system clipboard
-vim.keymap.set("n", "<leader>y", '"+y', { silent = true, desc = "Yank to system clipboard" })
-vim.keymap.set("v", "<leader>y", '"+y', { silent = true, desc = "Yank to system clipboard" })
-vim.keymap.set("n", "<leader>p", '"+p', { silent = true, desc = "Paste from system clipboard" })
+map("n", "<leader>f", require("fzf-lua").files, { desc = "Find files" })
+map("n", "<leader>w", "<cmd>w<CR>", { silent = true, desc = "Save file" })
+map("n", "<leader>r", "<cmd>luafile ~/.config/nvim/init.lua<CR><cmd>echo 'Config reloaded'<CR>", { desc = "Reload config" })
+map("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>", { silent = true, desc = "Clear search highlights" })
 
--- moves
-vim.keymap.set({"n", "x"}, "<leader>h", "^")
-vim.keymap.set({"n", "x"}, "<leader>l", "$")
+map({"n", "x"}, "<leader>h", "^")
+map({"n", "x"}, "<leader>l", "$")
 
+map("n", "<leader>gg", "<cmd>Gitsigns toggle_signs<CR>", { desc = "Gitsigns: signs" })
+map("n", "<leader>gn", "<cmd>Gitsigns toggle_numhl<CR>", { desc = "Gitsigns: numhl" })
+map("n", "<leader>gl", "<cmd>Gitsigns toggle_linehl<CR>", { desc = "Gitsigns: linehl" })
+map("n", "<leader>gw", "<cmd>Gitsigns toggle_word_diff<CR>", { desc = "Gitsigns: word_diff" })
+map("n", "<leader>gd", "<cmd>Gitsigns toggle_deleted<CR>", { desc = "Gitsigns: deleted" })
+
+-- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
