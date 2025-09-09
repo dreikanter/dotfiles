8139046d344cbc0c595e58e9d56471a33fa4be19
Alex Musayev <alex.musayev@gmail.com>
2025-09-09 13:36:04 +0200

    Neovim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,6 +5,12 @@ vim.g.editorconfig = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.clipboard = "unnamedplus"
+vim.o.compatible = false
+vim.o.title = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.smartindent = true
+vim.o.showmode = false -- do not show mode, since statusline shows it anyway
 
 -- plugins
 
@@ -85,11 +91,14 @@ map("n", "<leader>-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 
 map({"n", "x"}, "<leader>h", "^")
 map({"n", "x"}, "<leader>l", "$")
+map("n", "<C-d>", "<C-d>zz") -- center cursor on screen after page-down, Ctrl-D
+map("n", "<C-u>", "<C-u>zz") -- center cursor on screen after page-up, Ctrl-U
 
 map("n", "<leader>gg", "<cmd>Gitsigns toggle_signs<CR>", { desc = "Gitsigns: signs" })
 map("n", "<leader>gn", "<cmd>Gitsigns toggle_numhl<CR>", { desc = "Gitsigns: numhl" })
 map("n", "<leader>gl", "<cmd>Gitsigns toggle_linehl<CR>", { desc = "Gitsigns: linehl" })
 map("n", "<leader>gw", "<cmd>Gitsigns toggle_word_diff<CR>", { desc = "Gitsigns: word_diff" })
 map("n", "<leader>gd", "<cmd>Gitsigns toggle_deleted<CR>", { desc = "Gitsigns: deleted" })
+map("v", "<leader>s", "\"hy:%s/<C-r>h//g<left><left>")
 
 -- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
